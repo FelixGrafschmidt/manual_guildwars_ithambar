88df27176e149e8280a28d7c9c65cd297c95c9cc
Felix Grafschmidt <felix.grafschmidt@gmail.com>
2024-05-23 20:47:13 +0200

    first steps in custom python logic; first edits via custom ui

--- src/hooks/Options.py
+++ src/hooks/Options.py
@@ -22,20 +22,57 @@ from ..Helpers import is_option_enabled, get_option_value
 # Then, to see if the option is set, you can call is_option_enabled or get_option_value.
 #####################################################################
 
+class PossibleStarts(Choice):
+    """Which campaigns do you want to include in your possible start locations?"""
+    display_name = "Possible Starts"
+    option_all = 0
+    option_prophecies = 1
+    option_factions = 2
+    option_nightfall = 3
+    option_prophecies_factions = 4
+    option_prophecies_nightfall = 5
+    option_factions_nightfall = 6
+    default = 6
 
-# To add an option, use the before_options_defined hook below and something like this:
-#   options["total_characters_to_win_with"] = TotalCharactersToWinWith
-#
-class TotalCharactersToWinWith(Range):
-    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
-    display_name = "Number of characters to beat the game with before victory"
-    range_start = 10
-    range_end = 50
-    default = 50
 
+class PossibleGoals(Choice):
+    """Which campaigns do you want to include in your possible goal locations?"""
+    display_name = "Possible Goals"
+    option_all = 0
+    option_prophecies = 1
+    option_prophecies_factions = 2
+    option_prophecies_nightfall = 3
+    option_prophecies_eotn = 4
+    option_prophecies_factions_nightfall = 5
+    option_prophecies_factions_eotn = 6
+    option_prophecies_nightfall_eotn = 7
+    option_factions = 8
+    option_factions_nightfall = 9
+    option_factions_eotn = 10
+    option_factions_nightfall_eotn = 11
+    option_nightfall = 12
+    option_nightfall_eotn = 13
+    option_eotn = 14
+    default = 0
+
+class ElonaReachBonus(Toggle):
+    """Should the Elona Reach bonus be enabled?"""
+    display_name = "Elona Reach Bonus"
+    default = False
+
+class StartingProfessionsAmount(Range):
+    """How many professions should players start with?"""
+    display_name = "Starting Professions Amount"
+    range_start = 1
+    range_end = 8
+    default = 1
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
+    options["possible_starts"] = PossibleStarts
+    options["possible_goals"] = PossibleGoals
+    options["elona_reach_bonus"] = ElonaReachBonus
+    options["starting_professions_amount"] = StartingProfessionsAmount
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
